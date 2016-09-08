b9d5f0ea2bc5c57eb5dc5bf92eb9df8ab4cccf39
matramos <matramos.89@gmail.com>
2016-09-08 03:04:55 -0300

    meti los metodos, hay que modificarlos.

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
@@ -4,13 +4,48 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity{
+
+    Button button;
+    SeekBar dias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        dias = (SeekBar) findViewById(R.id.seekBarDias);
+
+        button.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+
+            }
+        });
+
+        dias.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
     }
 
 }
